141c0777f7efc13e5843bcde50660351e603e726
Vitaly <supercyfqgth2010@yandex.ru>
2021-08-09 20:17:11 +0300

    fixed task2

--- task2/Worker.java
+++ task2/Worker.java
@@ -11,8 +11,12 @@ public class Worker {
 
     public void start() {
         for (int i = 0; i < 100; i++) {
-            callback.onDone("Task " + i + " is done");
-            if (i == 33) errorCallback.onError("Task " + i + " is not done");
+            if (i == 33) {
+                errorCallback.onError("Task " + i + " is not done");
+            } else {
+                callback.onDone("Task " + i + " is done");
+            }
+
         }
     }
 }
